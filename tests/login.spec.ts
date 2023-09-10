c3db4d496199864e26ba368ac7d03a1b05458ef1
Jufinka <justynafijor@gmail.com>
2023-09-10 14:52:07 +0200

    chore: test data of user mail

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,19 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-date/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD-R02 @S02', async ({ page }) => {
     // Arrange
-    const userEmail = 'Moses.Armstrong@Feest.ca';
+    const userEmail = testUser1.userEmail;
     const userPassword = 'test1';
     const loginPage = new LoginPage(page);
-    
+
     // Act
     await loginPage.goto();
     await loginPage.login(userEmail, userPassword);
-    
+
     const welcomePage = new WelcomePage(page);
     const title = await welcomePage.title();
 
