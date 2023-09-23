00c0327e9c8f0533e44224317ea498682f49a062
Jufinka <justynafijor@gmail.com>
2023-09-23 21:15:44 +0200

    wip: login refactor

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,12 @@
+export interface LoginUser {
+  userEmail: string;
+  userPassword: string
+}
 export interface RegisterUser {
   userFirstName: string;
   userLastName: string;
   userEmail: string;
   userPassword: string;
 }
+
+
