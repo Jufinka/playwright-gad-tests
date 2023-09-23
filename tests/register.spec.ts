7ef6a75084b6eb92e675d612e825b3f4a27bafc3
Jufinka <justynafijor@gmail.com>
2023-09-23 22:43:51 +0200

    chore: random user factory

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,8 +1,7 @@
-import { RegisterUser } from '../src/models/user.model';
+import { randomUserData } from '../src/factories/user.factory';
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
 import { WelcomePage } from '../src/pages/welcome.page';
-import { faker } from '@faker-js/faker/locale/en';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
@@ -10,27 +9,18 @@ test.describe('Verify register', () => {
     page,
   }) => {
     // Arrange
-    const registerUserData: RegisterUser = {
-      userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
-      userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
-      userEmail: '',
-      userPassword: faker.internet.password(),
-    };
-
-    registerUserData.userEmail = faker.internet.email({
-      firstName: registerUserData.userFirstName,
-      lastName: registerUserData.userLastName,
-    });
-
+    const registerUserData = randomUserData();
     const registerPage = new RegisterPage(page);
 
     // Act
     await registerPage.goto();
     await registerPage.register(registerUserData);
+
     const expectedAlertPopupText = 'User created';
 
     // Assert
     await expect(registerPage.alertPopup).toHaveText(expectedAlertPopupText);
+
     const loginPage = new LoginPage(page);
     await loginPage.waitForPageToLoadUrl();
     const titleLogin = await loginPage.title();
@@ -52,12 +42,8 @@ test.describe('Verify register', () => {
     page,
   }) => {
     // Arrange
-    const registerUserData: RegisterUser = {
-      userFirstName: faker.person.firstName().replace(/[^A-Za-z]/g, ''),
-      userLastName: faker.person.lastName().replace(/[^A-Za-z]/g, ''),
-      userEmail: '#$%',
-      userPassword: faker.internet.password(),
-    };
+    const registerUserData = randomUserData();
+    registerUserData.userEmail = '#$%';
 
     const expectedErrorText = 'Please provide a valid email address';
     const registerPage = new RegisterPage(page);
@@ -75,17 +61,14 @@ test.describe('Verify register', () => {
   }) => {
     // Arrange
     const expectedErrorText = 'This field is required';
+    const registerUserData = randomUserData();
     const registerPage = new RegisterPage(page);
 
     // Act
     await registerPage.goto();
-    await registerPage.userFirstNameInput.fill(
-      faker.person.firstName().replace(/[^A-Za-z]/g, ''),
-    );
-    await registerPage.userLastNameInput.fill(
-      faker.person.lastName().replace(/[^A-Za-z]/g, ''),
-    );
-    await registerPage.userPasswordInput.fill(faker.internet.password());
+    await registerPage.userFirstNameInput.fill(registerUserData.userFirstName);
+    await registerPage.userLastNameInput.fill(registerUserData.userLastName);
+    await registerPage.userPasswordInput.fill(registerUserData.userPassword);
     await registerPage.registerButton.click();
 
     // Assert
